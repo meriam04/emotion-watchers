96eb06dd68be3e1cfec1661e98b2760c2588f172
Colin Smith <colinsmith497@gmail.com>
2023-10-19 18:57:59 -0400

    Created a tool to convert a video to an image sequence

---
+++ data_processing/testing/test_video_to_images.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import pytest
+
+from video_to_images import extract_frames
+
+@pytest.mark.parametrize("test_video, test_rate, expected_num_images,",
+    [(Path(__file__).parent / "keyboard_cat.mp4", 1, 54),
+     (Path(__file__).parent / "keyboard_cat.mp4", 2, 109),
+     (Path(__file__).parent / "keyboard_cat.mp4", 3, 163),])
+def test_extract_frames(test_video, test_rate, expected_num_images):
+    image_paths = extract_frames(test_video, test_rate, Path(__file__).parent / "images")
+    assert len(image_paths) == expected_num_images
+
+    for image_path in image_paths:
+        assert image_path.exists() and image_path.is_file()
+
+@pytest.mark.parametrize("test_video,",
+    [("dne.mp4"),
+     (Path(__file__).parent),
+     (Path(__file__))])
+def test_nonexistent_video(test_video):
+    with pytest.raises(Exception):
+        extract_frames(test_video, 1, Path(__file__).parent / "images")
