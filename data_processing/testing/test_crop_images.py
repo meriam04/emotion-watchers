73c5d203344757013cce08056e1218f9d66bd7c4
Colin Smith <colinsmith497@gmail.com>
2023-11-09 13:57:03 -0500

    Resizing first draft

--- data_processing/testing/test_crop_images.py
+++ data_processing/testing/test_crop_images.py
@@ -1,19 +1,19 @@
 from pathlib import Path
 import pytest
 
-from ..crop_images import crop_image, crop_images
-from ..utils import Point, Region
+from ..crop_and_resize_images import crop_and_resize_image, crop_and_resize_images
+from ..utils import Point, Region, Resolution
 
 @pytest.mark.parametrize("test_image, test_region, expected_image,",
-    [(Path(__file__).parent / "happy_man.png", Region(Point(100, 100), Point(400, 400)), Path(__file__).parent / "cropped/happy_man_cropped.png"),])
-def test_crop_image(test_image, test_region, expected_image):
-    assert crop_image(test_image, test_region) == expected_image
+    [(Path(__file__).parent / "happy_man.png", Region(Point(100, 100), Point(400, 400)), Resolution(224, 224), Path(__file__).parent / "cropped/happy_man_c.png"),])
+def test_crop_and_resize_image(test_image, test_region, test_resolution, expected_image):
+    assert crop_and_resize_image(test_image, test_region, test_resolution) == expected_image
     assert expected_image.exists() and expected_image.is_file()
 
 @pytest.mark.parametrize("test_images, test_region, expected_images,",
-    [([Path(__file__).parent / "happy_man.png", Path(__file__).parent / "excited_woman.png"], Region(Point(100, 100), Point(324, 324)), [Path(__file__).parent / "cropped/happy_man_cropped.png", Path(__file__).parent / "cropped/excited_woman_cropped.png"]),])
-def test_crop_images(test_images, test_region, expected_images):
-    image_paths = crop_images(test_images, test_region)
+    [([Path(__file__).parent / "happy_man.png", Path(__file__).parent / "excited_woman.png"], Region(Point(100, 100), Point(500, 500)), Resolution(224, 224), [Path(__file__).parent / "cropped/happy_man_c.png", Path(__file__).parent / "cropped/excited_woman_c.png"]),])
+def test_crop_and_resize_images(test_images, test_region, test_resolution, expected_images):
+    image_paths = crop_and_resize_images(test_images, test_region, test_resolution)
     assert len(image_paths) == len(expected_images)
 
     for i, image_path in enumerate(image_paths):
