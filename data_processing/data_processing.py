ff6c84739a129553dbc749f811c5e0eaafc7176b
Colin Smith <colinsmith497@gmail.com>
2024-01-04 16:07:05 -0500

    Implemented baseline model
    Created a tool to convert a video to an image sequence
    
    Created tool for cropping images to a specified region
    
    Updated testing to tests, and added a .gitignore file
    
    Resizing first draft
    
    Added resizing tool
    
    Fixed test_video_to_images.py
    
    Processed videos from the first experiment
    
    Revert "Processed videos from the first experiment"
    
    Resizing first draft
    
    Added resizing tool
    
    Fixed test_video_to_images.py
    
    Resizing first draft
    
    Added resizing tool
    
    Fixed data processing script
    
    Added weights to gitignore
    
    Temporarily updated .gitignore
    
    Deleted junk
    
    Undid temporary change to .gitignore

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python3
+
 from pathlib import Path
+import sys
 from typing import List
 
 from .crop_and_resize_images import crop_and_resize_images
 from .utils import Point, Region, Resolution
 from .video_to_images import extract_frames
 
+RATE = 1
 TOP_LEFT = Point(100, 100)
 BOTTOM_RIGHT = Point(324, 324)
 RESOLUTION = Resolution(224, 224)
 
-def data_processing(video_paths: List[Path]) -> List[Path]:
-    for video in video_paths:
-        image_paths = extract_frames(video, 1)
-        image_paths = crop_and_resize_images(image_paths, Region(TOP_LEFT, BOTTOM_RIGHT), RESOLUTION)
+def data_processing(video_path: Path, output_path: Path) -> List[Path]:
+    image_paths = extract_frames(video_path, RATE, output_path)
+    return crop_and_resize_images(image_paths, Region(TOP_LEFT, BOTTOM_RIGHT), RESOLUTION)
+
+if __name__ == "__main__":
+    data_processing(Path(sys.argv[1]), Path(sys.argv[2]))
