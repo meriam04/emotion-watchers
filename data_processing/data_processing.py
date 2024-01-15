d6d3ca80126f99ec025f56f28afe441681833a44
divyaK15 <divya.p.k15@gmail.com>
2024-01-15 15:35:19 -0500

    adding comments for testing pipeline

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -8,10 +8,12 @@ from .crop_and_resize_images import crop_and_resize_images
 from .utils import Point, Region, Resolution
 from .video_to_images import extract_frames
 
+#If we are making a UI, return these values for TOP_LEFT and BOTTOM_RIGHT 
 RATE = 1
 TOP_LEFT = Point(100, 100)
 BOTTOM_RIGHT = Point(324, 324)
 RESOLUTION = Resolution(224, 224)
+#Add FILE_LIST as global variable 
 
 def data_processing(video_path: Path, output_path: Path) -> List[Path]:
     image_paths = extract_frames(video_path, RATE, output_path)
@@ -19,3 +21,5 @@ def data_processing(video_path: Path, output_path: Path) -> List[Path]:
 
 if __name__ == "__main__":
     data_processing(Path(sys.argv[1]), Path(sys.argv[2]))
+
+#Adding a function to move the images to the corresponding directory 
